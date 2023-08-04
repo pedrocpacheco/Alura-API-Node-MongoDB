93122e855e0c5a0d09a1f78c2da035c5857adfb1
pedrocpacheco <cpedropacheco@hotmail.com>
2023-08-04 19:21:47 -0300

    fat: community controller

---
+++ src/controllers/communityController.js
@@ -0,0 +1,50 @@
+import communityModel from "../models/Community.js";
+
+class CommunityController {
+
+    static findAll = (req, res) =>{
+        communityModel.find((err, communityModel) =>{
+            res.send(communityModel);
+        })
+    }
+
+    static findById = (req, res) => {
+        communityModel.findById((err, communityModel) => {
+            if(!err){ res.send(communityModel); }
+            else{ res.status(404).send({ error: "Community not founded" }) }
+        })
+    }
+
+    static save = (req, res) => {
+        let community = new communityModel(req.body);
+        community.save((err, communityModel) => {
+            if(!err) { res.send(communityModel) }
+            else { res.status(500).send({ error: `Error saving the community: ${err.message}` }) }
+        })
+    }
+
+    static update = (req, res) => {
+        let id = req.params.id;
+        communityModel.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+            if(!err){
+                res.send({ message: "Community updated!" })
+            }else{
+                res.status(500).send({ error: `Error to Update: ${err.message}` })
+            }
+        });
+    }
+
+    static delete = (req, res) => { 
+        let id = req.params.id;
+        communityModel.findByIdAndRemove(id, (err) => {
+            if(!err){
+                req.send({ message: "Community removed!" });
+            } else{
+                req.send({ error: `Error to Delete: ${err.message}` })
+            }
+        })
+    }
+
+}
+
+export default CommunityController;
