82a7b14b993152e83bdfe9c768d47ff9c778b61e
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-04 16:19:37 -0300

    feat: another methods

--- src/app.js
+++ src/app.js
@@ -1,29 +1,56 @@
 import express from "express";
 
 const app = express();
+app.use(express.json())
 
 const books = [
     { id: 1, "title": "Lord of the Rings" },
     { id: 2, "title": "Hobbit" }
 ];
 
+// * GET: Hello World
 app.get("/", (req, res) =>{
     return res.status(200).send("Hello World!");
 });
 
-app.get("/livros", (req, res) =>{
+// * GET: Books List
+app.get("/books", (req, res) =>{
     return res.json(books);
 });
 
-app.post("/livros", (req, res) => {
+// ? POST: Create Book
+app.post("/books", (req, res) => {
     const { id, title } = req.body;
-
     const book = { id, title }
-
     books.push(book);
 
     res.status(201).json(book);
-
 });
+
+//TODO PUT: Change Book by ID 
+app.put("/books/:id", (req, res) =>{
+    const { title } = req.body;
+    const book = { title }
+
+    let index = findBooks(req.params.id);
+    books[index] = book;
+
+    res.status(201).json(book);
+});
+
+// * GET: Book by ID
+app.get("/books/:id", (req, res) =>{
+    let index = findBooks();
+    if(index !== -1) {
+        const book = books[index];
+        res.json(book);
+    }else{
+        res.status(404).json({ error: "Book not founded" })
+    }
+});
+
+function findBooks(id){
+    return books.findIndex(book => book.id === id);
+}
 
 export default app;
