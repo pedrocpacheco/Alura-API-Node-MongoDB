0ae7f199edc7cbbcaf2013b733eac24e17d9fa5e
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-04 15:36:04 -0300

    feat: using a object to simulate routes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 const http = require("http");
 
 const PORT = 3000;
+const routes = {
+    "/": "Hello World!",
+    "/books": "List of Books",
+    "/authors": "Our Authors"
+}
 
 const server = http.createServer((req, res) =>{
     res.writeHead(200, { "Content-Type": "text/plain" })
-    res.end("Hello World!")
+    res.end(routes[req.url])
 });
 
 server.listen(PORT, () => console.log(`Server Listening PORT: http://localhost:${PORT}`));
