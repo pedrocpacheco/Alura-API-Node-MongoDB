12d49b58d0bfbc8935bfdcf781a2d0d9986d382f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-04 17:30:58 -0300

    feat: connection to MongoDB

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import express from "express";
+import db from "./config/dbConnect.js"
+
+db.on("error", console.log.bind(console, "Error to connect to DB"));
+db.once("open", () => { console.log("DB Connection was succedded") });
 
 const app = express();
 app.use(express.json())
